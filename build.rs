5ab368118a884bd8e656580cf424225d48683b61
Pooya Eimandar <Pooya.Eimandar@gmail.com>
2025-04-13 23:23:48 +0400

    fix: build time

--- build.rs
+++ build.rs
@@ -3,12 +3,13 @@ use chrono::{Datelike, Timelike, Utc};
 fn main() {
     let now = Utc::now();
     let version = format!(
-        "v{:04}.{:02}.{:02}.{:02}{:02}",
+        "v{:04}.{:02}.{:02}.{:02}{:02}{:02}",
         now.year(),
         now.month(),
         now.day(),
         now.hour(),
-        now.minute()
+        now.minute(),
+        now.second()
     );
     println!("cargo:rustc-env=SIB_BUILD_VERSION={}", version);
 }
