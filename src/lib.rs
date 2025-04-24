f3684cc73423a5d354c40a50b5fcdffa42d6bb89
Pooya Eimandar <Pooya.Eimandar@gmail.com>
2025-04-24 07:44:31 +0400

    fix: build

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+cfg_if::cfg_if! {
+    if #[cfg(any(target_os = "android", target_os = "ios"))] {
+        #[global_allocator]
+        static GLOBAL: std::alloc::System = std::alloc::System;
+    } else {
+        extern crate tikv_jemallocator;
+        #[global_allocator]
+        static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+    }
+}
 
 #[cfg(feature = "http-server")]
 pub mod network;
