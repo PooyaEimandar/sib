e55a7f545f0763197d8141dd9ccb8938a22f6add
Pooya Eimandar <Pooya.Eimandar@gmail.com>
2025-06-05 10:37:10 +0400

    fix: build

--- src/network/http/mod.rs
+++ src/network/http/mod.rs
@@ -1,7 +1,11 @@
-#[cfg(feature = "h1-server")]
-pub mod h1;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "h1-server")] {
+        pub mod h1;
+        pub mod message;
+        pub mod reader;
+        pub mod session;
+    }
+}
+
 #[cfg(feature = "h3-server")]
 pub mod h3;
-pub mod message;
-pub mod reader;
-pub mod session;
