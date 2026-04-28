41ddf87e7ac372c4e805e80b0cb002d4032a064f
Aria <aria3ppp@gmail.com>
2026-04-28 18:13:38 +0330

    Update db.rs via web browser

--- src/database/fdb/db.rs
+++ src/database/fdb/db.rs
@@ -1,3 +1,5 @@
+use foundationdb_sys::{FDBDatabaseOption, fdb_database_set_option};
+
 #[derive(Clone, Debug)]
 pub struct FDB {
     pub db: *mut foundationdb_sys::FDBDatabase,
@@ -8,15 +10,39 @@ unsafe impl Send for FDB {}
 unsafe impl Sync for FDB {}
 
 impl FDB {
-    pub fn new(cluster_file_path: &str) -> std::io::Result<Self> {
+    pub fn new(
+        cluster_file_path: &str,
+        options: &[(FDBDatabaseOption, &[u8])],
+    ) -> std::io::Result<Self> {
         let mut db: *mut foundationdb_sys::FDBDatabase = std::ptr::null_mut();
         let c_path = std::ffi::CString::new(cluster_file_path)
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidInput, e))?;
         let res = unsafe { foundationdb_sys::fdb_create_database(c_path.as_ptr(), &mut db) };
         if res != 0 {
             return Err(std::io::Error::other(format!(
-                "Failed to setup FoundationDB network: {res}"
+                "Failed to setup FoundationDB network: {}",
+                super::fdb_err(res)
             )));
+        }
+        for (option, value) in options {
+            let value_ptr = if value.is_empty() {
+                std::ptr::null()
+            } else {
+                value.as_ptr()
+            };
+
+            let err = unsafe {
+                fdb_database_set_option(db, *option, value_ptr, value.len() as std::os::raw::c_int)
+            };
+
+            if err != 0 {
+                return Err(std::io::Error::other(format!(
+                    "Failed to set option `{}` with value `{}`: {}",
+                    *option,
+                    String::from_utf8_lossy(value),
+                    super::fdb_err(err)
+                )));
+            }
         }
         Ok(Self { db })
     }
